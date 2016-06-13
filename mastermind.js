690c81b9a281301f6fae2b17c53801779f0b1f58
dapenguin <anthony.jeffery@littleblueweb.com>
2016-06-13 12:13:40 +0100

    Added player name

--- mastermind.js
+++ mastermind.js
@@ -8,6 +8,9 @@ Mastermind = function(){
 	var _colours = ['red','blue','yellow','black','white','green'];
 
 	// The code the player has to crack
+	var _playerName = 'Anthony';
+
+	// The code the player has to crack
 	var _secretCode = [];
 
 	// Maximum number of tries
